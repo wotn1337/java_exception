8bcb89b71ea8b97dbcc79cb23d37cb64468e76a3
pavlov24 <pavlovmark24@gmail.com>
2023-11-09 10:11:20 +0500

    add first & second

--- src/main/java/exception/First.java
+++ src/main/java/exception/First.java
@@ -1,2 +1,40 @@
-package exception;public class First {
+package exception;
+
+public class First {
+
+    public static void main(String[] args) {
+        divideByZero();
+        accessToUnexistingElement();
+        playWithNullPointer();
+        tryToCreateFunnyArray();
+        searchSomethingOutString();
+        System.out.println("Ура! Меня снова печают");
+    }
+
+
+    public static void divideByZero() {
+        int a = 777 / (666 - 666);
+    }
+
+    public static void accessToUnexistingElement(){
+        int[] arr = {1,2,3,4,5,6,7};
+        for (int i = 0; i < arr.length+1; i++) {
+            System.out.printf("%d,", i);
+        }
+    }
+
+    public static void playWithNullPointer() {
+        String importantData = null;
+        System.out.println("important is " + importantData);
+    }
+
+    public static void tryToCreateFunnyArray() {
+        int i[] = new int[-5];
+    }
+
+    public static void searchSomethingOutString() {
+        String secret = "Пин-код от карты: 123";
+        secret.charAt(22);
+    }
+
 }
--- src/main/java/exception/Second.java
+++ src/main/java/exception/Second.java
@@ -1,2 +1,9 @@
-package exception;public class Second {
+package exception;
+
+public class Second {
+
+    public static void main(String[] args) {
+        // todo write demo
+    }
+
 }
